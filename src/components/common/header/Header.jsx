e968f75e2cc460613165811a9b90258af857cbe9
zero <zero@zerosrat.com>
2017-12-25 21:54:35 +0800

    通用组件header完成

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,20 +1,31 @@
 // @flow
 import React from 'react'
-import logo from 'src/assets/logo.png'
+import { Link } from 'react-router-dom'
 import './header.css'
-
-// const Header = () => (
-//   <header styleName="header">
-//     <img src={logo} styleName="logo" alt="logo" />
-//     <h1 styleName="title">Welcome to React</h1>
-//   </header>
-// )
+import logo from 'src/assets/logo.png'
+import avatar from 'src/assets/avatar1.png'
 
 class Header extends React.PureComponent<{}> {
   render() {
     return (
-      <header>
-        <img src={logo} />
+      <header styleName="header">
+        <div styleName="header-inner">
+          <a href="/"><img src={logo} /></a>
+          <nav styleName="header-nav">
+            <Link styleName="header-navItem" to="/">首页</Link>
+            <Link styleName="header-navItem" to="/">人才</Link>
+            <Link styleName="header-navItem" to="/">项目</Link>
+            <Link styleName="header-navItem" to="/">知识产权</Link>
+          </nav>
+          <div styleName="header-userInfo">
+            <div styleName="header-profile">
+              <img styleName="header-avatar" src={avatar} />
+              <div styleName="header-username">张老师</div>
+            </div>
+            <span styleName="separator" />
+            <div styleName="header-logout">登出</div>
+          </div>
+        </div>
       </header>
     )
   }
