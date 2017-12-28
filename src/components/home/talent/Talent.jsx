be91dd49dc96e93e83178071ca6dc1ef7777a5f3
zero <zero@zerosrat.com>
2017-12-28 19:56:33 +0800

    主页：人才库组件样式完成

--- src/components/home/talent/Talent.jsx
+++ src/components/home/talent/Talent.jsx
@@ -1,11 +1,21 @@
 // @flow
 import React from 'react'
 import './talent.css'
+import TalentHeader from './header/TalentHeader'
+import TalentItem from './talent-item/TalentItem'
 
 export default class Talent extends React.Component<{}> {
   render() {
+    const talentItems = [null, null, null, null, null].map((item, idx) => {
+      return <li key={idx}><TalentItem /></li>
+    })
     return (
-      <div styleName="talent">talent</div>
+      <div styleName="talent">
+        <TalentHeader />
+        <ul styleName="talent-items">
+          {talentItems}
+        </ul>
+      </div>
     )
   }
 }
