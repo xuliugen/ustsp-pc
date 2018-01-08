37a10e53bad5876aec6cbc8e1e0a23deeb033253
zero <zero@zerosrat.com>
2018-01-08 15:34:21 +0800

    主页：项目修改 细节
    1. 数据常量改为状态
    2. list render添加key
    3. 为子组件传递整个obj，而不是obj的attr

--- src/components/home/project/project-item/ProjectItem.jsx
+++ src/components/home/project/project-item/ProjectItem.jsx
@@ -3,32 +3,33 @@ import React from 'react'
 import './projectItem.css'
 import imgSee from './see.png'
 
-type Props = {
+type ProjectObj = {
   title: string,
-  school: string,
-  startTime: string,
-  endTime: string,
-  visitNum: number,
-  price: number,
-  bgColor: string
+  school: string
+  // ...
+}
+
+type Props = {
+  project: ProjectObj
 }
 
 export default class ProjectItem extends React.Component<Props> {
   render() {
+    const { project } = this.props
     return (
       <div styleName="project-item">
         <div styleName="item">
           <div styleName="head">
-            <div styleName="title">{this.props.title}</div>
-            <div styleName="school" style={{backgroundColor: this.props.bgColor}} >{this.props.school}</div>
+            <div styleName="title">{project.title}</div>
+            <div styleName="school" style={{backgroundColor: project.bgColor}} >{project.school}</div>
           </div>
           <div styleName="time">
-            <span>发布于 {this.props.startTime}</span>
-            <span>截止于 {this.props.endTime}</span>
-            <span><img src={imgSee} /> {this.props.visitNum}</span>
+            <span>发布于 {project.startTime}</span>
+            <span>截止于 {project.endTime}</span>
+            <span><img src={imgSee} /> {project.visitNum}</span>
           </div>
         </div>
-        <div styleName="price">¥<span>{this.props.price}</span></div>
+        <div styleName="price">¥<span>{project.price}</span></div>
       </div>
     )
   }
