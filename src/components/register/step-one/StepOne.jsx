2f885e3f82b11e0bb416550adb743738263af823
zero <zero@zerosrat.com>
2018-01-08 20:24:31 +0800

    注册：第一步初始化

--- src/components/register/step-one/StepOne.jsx
+++ src/components/register/step-one/StepOne.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import './stepOne.css'
 
 export default class StepOne extends React.Component<{}> {
   render() {
     return (
-      <div>s1</div>
+      <div styleName="container">
+        <div styleName="title">step 1 : 填写基本信息</div>
+        <form styleName="form">
+          <div styleName="row">
+            <select>
+              <option value="1">1</option>
+            </select>
+          </div>
+          <div styleName="row">
+            <input />
+          </div>
+          <div styleName="row">
+            <input />
+          </div>
+          <div styleName="row">
+            <input />
+          </div>
+          <div styleName="row">
+            <input />
+          </div>
+          <div styleName="row">
+            <input />
+          </div>
+          <div styleName="row">
+            <span>我已阅读并同意《UppFind用户注册协议》</span>
+          </div>
+          <div styleName="nextBtn-container">
+            <button styleName="nextBtn">下一步</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
--- src/views/register/Register.jsx
+++ src/views/register/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import { RegisterHeader, StepOne, StepTwo, StepThree } from 'components/register'
+import { RegisterHeader, Progress, StepOne, StepTwo, StepThree } from 'components/register'
 import { Footer } from 'components/common'
 import './register.css'
 
@@ -11,11 +11,16 @@ export default class Register extends React.Component<{}> {
     return (
       <div styleName="register">
         <RegisterHeader />
-        <Switch>
-          <Route path={`${match.url}/1`} component={StepOne} />
-          <Route path={`${match.url}/2`} component={StepTwo} />
-          <Route path={`${match.url}/3`} component={StepThree} />
-        </Switch>
+        <div styleName="progress-wrapper">
+          <Progress />
+        </div>
+        <div styleName="content-wrapper">
+          <Switch>
+            <Route path={`${match.url}/1`} component={StepOne} />
+            <Route path={`${match.url}/2`} component={StepTwo} />
+            <Route path={`${match.url}/3`} component={StepThree} />
+          </Switch>
+        </div>
         <div styleName="footer-wrapper">
           <Footer />
         </div>
