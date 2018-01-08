cca2c3f788bf3e0b1c54d1693cb2876662ca6978
zero <zero@zerosrat.com>
2018-01-08 15:34:21 +0800

    主页：项目修改 细节
    1. 数据常量改为状态
    2. list render添加key
    3. 为子组件传递整个obj，而不是obj的attr

--- src/components/home/project/Project.jsx
+++ src/components/home/project/Project.jsx
@@ -4,29 +4,43 @@ import './project.css'
 import ProjectHeader from './header/Header'
 import ProjectItem from './project-item/ProjectItem'
 
-const ITEMS: Array<{}> = [
-  {title: '项目标题', school: '电子科技大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 1346, price: 12000, bgColor: '#1dbbae'},
-  {title: '第二个项目标题', school: '西南石油大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 890, price: 9000, bgColor: '#2b7dd6'},
-  {title: 'An English title project', school: '四川大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 890, price: 10000, bgColor: '#1dbbae'},
-  {title: '长度为10个字以上的标题样式', school: '西南民族大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 3000, price: 500, bgColor: '#2b7dd6'},
-  {title: '某一个标题', school: '成都大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 12, price: 500, bgColor: '#1dbbae'}
-]
+type ProjectObj = {
+  title: string,
+  school: string
+  // ...
+}
 
-export default class Project extends React.Component<{}> {
+type State = {
+  projects: Array<ProjectObj>
+}
+
+export default class Project extends React.Component<{}, State> {
+  constructor() {
+    super()
+    this.state = {
+      projects: [
+        { title: '项目标题', school: '电子科技大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 1346, price: 12000, bgColor: '#1dbbae' },
+        { title: '第二个项目标题', school: '西南石油大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 890, price: 9000, bgColor: '#2b7dd6' },
+        { title: 'An English title project', school: '四川大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 890, price: 10000, bgColor: '#1dbbae' },
+        { title: '长度为10个字以上的标题样式', school: '西南民族大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 3000, price: 500, bgColor: '#2b7dd6' },
+        { title: '某一个标题', school: '成都大学', startTime: '2017-11-30', endTime: '2017-12-30', visitNum: 12, price: 500, bgColor: '#1dbbae' }
+      ]
+    }
+  }
+
   render() {
-    const projectItem = ITEMS.map((item) => {
-      return (
-        <div styleName="project-item-wrapper">
-          <ProjectItem title={item.title} school={item.school} startTime={item.startTime} endTime={item.endTime} visitNum={item.visitNum} price={item.price} bgColor={item.bgColor} />
-        </div>
-      )
-    })
     return (
       <div styleName="project">
         <div styleName="header-wrapper">
           <ProjectHeader />
         </div>
-        {projectItem}
+        {this.state.projects.map((item, idx) => {
+          return (
+            <div styleName="project-item-wrapper" key={idx}>
+              <ProjectItem project={item} />
+            </div>
+          )
+        })}
       </div>
     )
   }
