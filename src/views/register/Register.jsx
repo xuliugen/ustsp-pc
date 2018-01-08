f69c6fa51818ad709b95c74678fddbe0756ada59
zero <zero@zerosrat.com>
2018-01-08 19:49:37 +0800

    注册：header完成

--- src/views/register/Register.jsx
+++ src/views/register/Register.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import { RegisterHeader, StepOne, StepTwo, StepThree } from 'components/register'
+import { Footer } from 'components/common'
+import './register.css'
 
 export default class Register extends React.Component<{}> {
   render() {
     const { match } = this.props
     return (
-      <div>
+      <div styleName="register">
         <RegisterHeader />
         <Switch>
           <Route path={`${match.url}/1`} component={StepOne} />
           <Route path={`${match.url}/2`} component={StepTwo} />
           <Route path={`${match.url}/3`} component={StepThree} />
         </Switch>
+        <div styleName="footer-wrapper">
+          <Footer />
+        </div>
       </div>
     )
   }
