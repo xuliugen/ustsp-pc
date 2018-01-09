e8a437b724b3e42f259987887d87421a6d14fb3b
wang <374951963@qq.com>
2018-01-09 20:47:29 +0800

    注册: 完成注册第二步结构样式

--- src/components/register/step-two/StepTwo.jsx
+++ src/components/register/step-two/StepTwo.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './stepTow.css'
+import imgAvatar from 'src/assets/avatar1.png'
 
 export default class StepTwo extends React.Component<{}> {
   render() {
     return (
-      <div>s2</div>
+      <div styleName="container">
+        <div styleName="title">step2:检测到相关已有资料，可以进行认领。</div>
+        <div styleName="introduce">
+          <div styleName="toast">这是您本人吗?</div>
+          <img styleName="photo" src={imgAvatar} alt="这是头像" />
+          <div styleName="name">王福德</div>
+          <div styleName="profession">西南石油大学 / 电气工程专业</div>
+        </div>
+        <div styleName="button-operation">
+          <button styleName="no-button">不是</button>
+          <button styleName="next-button">是的，下一步</button>
+        </div>
+      </div>
     )
   }
 }
