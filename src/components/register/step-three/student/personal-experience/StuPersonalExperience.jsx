b8dbda14f641926aa11ff64cfece5d622eef0370
zero <zero@zerosrat.com>
2018-01-11 18:53:15 +0800

    注册：step3学生教育经历修改

--- src/components/register/step-three/student/personal-experience/StuPersonalExperience.jsx
+++ src/components/register/step-three/student/personal-experience/StuPersonalExperience.jsx
@@ -12,9 +12,9 @@ export default class PersonalExperience extends React.Component {
     return (
       <div styleName="personal-experience">
         <FormTitle title={'个人履历'} />
-        <Row>
-          <Col span={12} >
-            <Form styleName="personal-experience-item" style={{marginRight: '10px'}} layout="vertical" >
+        <Form styleName="personal-experience-item" style={{ marginRight: '10px' }} layout="vertical" >
+          <Row gutter={20}>
+            <Col span={12} >
               <FormItem label="现读学校" style={{ flexFlow: '1' }}>
                 <Select defaultValue="lucy" style={{ width: '100%' }} >
                   <Option value="jack">Jack</Option>
@@ -29,11 +29,9 @@ export default class PersonalExperience extends React.Component {
                   <Option value="Yiminghe">yiminghe</Option>
                 </Select>
               </FormItem>
-            </Form>
-          </Col>
-          <Col span={12}>
-            <Form styleName="personal-experience-item" style={{marginLeft: '10px'}} layout="vertical" >
-              <FormItem label="专业">
+            </Col>
+            <Col span={12}>
+              <FormItem label="学院">
                 <Select defaultValue="lucy" style={{ width: '100%' }} >
                   <Option value="jack">Jack</Option>
                   <Option value="lucy">Lucy</Option>
@@ -47,16 +45,18 @@ export default class PersonalExperience extends React.Component {
                   <Option value="Yiminghe">yiminghe</Option>
                 </Select>
               </FormItem>
-            </Form>
-          </Col>
-        </Row>
-        <FormItem label="擅长技能">
-          <Input size="Large" />
-        </FormItem>
-        <FormItem label="个人简介">
-          <TextArea rows={8} />
-          <div styleName="word-limit">字数限制: 0/400</div>
-        </FormItem>
+            </Col>
+          </Row>
+          <FormItem label="擅长技能">
+            <Input size="large" />
+          </FormItem>
+          <FormItem label="个人简介">
+            <div styleName="intro-container">
+              <TextArea rows={8} />
+              <span styleName="word-limit">字数限制: 0/400</span>
+            </div>
+          </FormItem>
+        </Form>
       </div>
     )
   }
