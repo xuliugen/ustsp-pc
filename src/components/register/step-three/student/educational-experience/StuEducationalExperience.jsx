5ae6f122db64edd5edb614a182f1a37616ce9cf6
wang <374951963@qq.com>
2018-01-11 17:22:34 +0800

    注册: 第三步中抽出学生教育经历的已认领组件

--- src/components/register/step-three/student/educational-experience/StuEducationalExperience.jsx
+++ src/components/register/step-three/student/educational-experience/StuEducationalExperience.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { FormTitle } from '../../common'
 import './stuEducationalExperience.css'
-import { Form, Input, Row, Col, Select } from 'antd'
-
-const FormItem = Form.Item
-const Option = Select.Option
+import ExpItem from './exp-item/ExpItem'
 
 export default class StuEdicationalExperience extends React.Component<{}> {
   render() {
@@ -12,50 +9,7 @@ export default class StuEdicationalExperience extends React.Component<{}> {
       <div styleName="educational-experience">
         <FormTitle title={'教育经历'} />
         <div styleName="content">
-          <div>
-            <Form layout="vertical">
-              <FormItem label="学历级别" styleName="form-item" >
-                <Input styleName="input-height" />
-              </FormItem>
-              <FormItem label="学校" styleName="form-item">
-                <Input styleName="input-height" />
-              </FormItem>
-              <FormItem label="学院" styleName="form-item">
-                <Input styleName="input-height" />
-              </FormItem>
-              <FormItem label="专业" styleName="form-item">
-                <Input styleName="input-height" />
-              </FormItem>
-            </Form>
-            <Row styleName="form-item">
-              <Col span={12} >
-                <Form styleName="" style={{ marginRight: '10px' }} layout="vertical" >
-                  <FormItem label="入学时间" style={{ flexFlow: '1' }}>
-                    <Select defaultValue="lucy" size="large" >
-                      <Option value="jack">Jack</Option>
-                      <Option value="lucy">Lucy</Option>
-                      <Option value="Yiminghe">yiminghe</Option>
-                    </Select>
-                  </FormItem>
-                </Form>
-              </Col>
-              <Col span={12}>
-                <Form styleName="" style={{ marginLeft: '10px' }} layout="vertical" >
-                  <FormItem label="毕业时间">
-                    <Select defaultValue="lucy" size="large" >
-                      <Option value="jack">Jack</Option>
-                      <Option value="lucy">Lucy</Option>
-                      <Option value="Yiminghe">yiminghe</Option>
-                    </Select>
-                  </FormItem>
-                </Form>
-              </Col>
-            </Row>
-            <div>
-              <button styleName="store-button">保存</button>
-              <button styleName="cancel-button">取消</button>
-            </div>
-          </div>
+          <ExpItem />
           <div>
             <div>
               <div>
---
+++ src/components/register/step-three/student/educational-experience/exp-item/ExpItem.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './expItem.css'
+import { Form, Input, Row, Col, Select } from 'antd'
+
+const FormItem = Form.Item
+const Option = Select.Option
+
+export default class ExpItem extends React.Component<{}> {
+  render() {
+    return (
+      <div>
+        <Form layout="vertical">
+          <FormItem label="学历级别" styleName="form-item" >
+            <Input styleName="input-height" />
+          </FormItem>
+          <FormItem label="学校" styleName="form-item" >
+            <Input styleName="input-height" />
+          </FormItem>
+          <FormItem label="学院" styleName="form-item" >
+            <Input styleName="input-height" />
+          </FormItem>
+          <FormItem label="专业" styleName="form-item" >
+            <Input styleName="input-height" />
+          </FormItem>
+        </Form>
+        <Row styleName="form-item">
+          <Col span={12} >
+            <Form styleName="" style={{ marginRight: '10px' }} layout="vertical" >
+              <FormItem label="入学时间" style={{ flexFlow: '1' }}>
+                <Select defaultValue="lucy" size="large" >
+                  <Option value="jack">Jack</Option>
+                  <Option value="lucy">Lucy</Option>
+                  <Option value="Yiminghe">yiminghe</Option>
+                </Select>
+              </FormItem>
+            </Form>
+          </Col>
+          <Col span={12}>
+            <Form styleName="" style={{ marginLeft: '10px' }} layout="vertical" >
+              <FormItem label="毕业时间">
+                <Select defaultValue="lucy" size="large" >
+                  <Option value="jack">Jack</Option>
+                  <Option value="lucy">Lucy</Option>
+                  <Option value="Yiminghe">yiminghe</Option>
+                </Select>
+              </FormItem>
+            </Form>
+          </Col>
+        </Row>
+        <div>
+          <button styleName="store-button">保存</button>
+          <button styleName="cancel-button">取消</button>
+        </div>
+      </div>
+    )
+  }
+}
