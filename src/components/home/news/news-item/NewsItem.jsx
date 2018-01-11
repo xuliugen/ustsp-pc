31922fe7896dac1dbb844e3d31bca8c79818d4e1
unknown <953847229@qq.com>
2018-01-11 09:45:11 +0800

    主页：最新动态细节修改

--- src/components/home/news/news-item/NewsItem.jsx
+++ src/components/home/news/news-item/NewsItem.jsx
@@ -3,25 +3,30 @@ import React from 'react'
 import './newsItem.css'
 import imgTouxiang from './touxiang.png'
 
-type Props = {
+type NewsObj = {
   name: string,
   company: string,
   time: string,
   content: string
 }
 
+type Props = {
+  news: NewsObj
+}
+
 export default class NewsItem extends React.Component<Props> {
   render() {
+    const { news } = this.props
     return (
       <div styleName="news-item">
         <div styleName="publisher">
           <span styleName="touxiang"><img src={imgTouxiang} /></span>
-          <span styleName="name">{this.props.name}</span>
-          <span styleName="company">{this.props.company}</span>
-          <span styleName="time">{this.props.time}</span>
+          <span styleName="name">{news.name}</span>
+          <span styleName="company">{news.company}</span>
+          <span styleName="time">{news.time}</span>
         </div>
         <div styleName="content">
-          <span styleName="text">{this.props.content}</span>
+          <span styleName="text">{news.content}</span>
         </div>
       </div>
     )
