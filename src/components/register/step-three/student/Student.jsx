2fef7c8008eddeec94ce75605176979889cb3b98
wang <374951963@qq.com>
2018-01-11 15:35:05 +0800

    注册: 第三步中抽出教育经历组件

--- src/components/register/step-three/student/Student.jsx
+++ src/components/register/step-three/student/Student.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './student.css'
 import StuBaseInfo from './base-info/StuBaseInfo'
 import StuPersonalExperience from './personal-experience/StuPersonalExperience'
+import StuEducationalExperience from './educational-experience/StuEducationalExperience'
 
 export default class StepThree extends React.Component<{}> {
   render() {
@@ -14,11 +15,7 @@ export default class StepThree extends React.Component<{}> {
         <div styleName="form-container">
           <StuBaseInfo />
           <StuPersonalExperience />
-        </div>
-        <div styleName="educational-experience">
-          <div styleName="divider">
-            <span styleName="base-introduce-title">教育经历</span>
-          </div>
+          <StuEducationalExperience />
         </div>
       </div>
     )
---
+++ src/components/register/step-three/student/educational-experience/StuEducationalExperience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FormTitle } from '../../common'
+import './stuEducationalExperience.css'
+
+export default class StuEdicationalExperience extends React.Component<{}> {
+  render() {
+    return (
+      <div styleName="educational-experience">
+        <FormTitle title={'教育经历'} />
+        <div>adffasdfsa  </div>
+      </div>
+    )
+  }
+}
