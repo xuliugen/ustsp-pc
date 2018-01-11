14491f69273a4e1e203190e9538b8b238fd263c0
wang <374951963@qq.com>
2018-01-11 17:28:46 +0800

    注册: 抽出第三步中学生教育经历的添加中组件

--- src/components/register/step-three/student/educational-experience/StuEducationalExperience.jsx
+++ src/components/register/step-three/student/educational-experience/StuEducationalExperience.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FormTitle } from '../../common'
 import './stuEducationalExperience.css'
 import ExpItem from './exp-item/ExpItem'
+import NewExpItem from './new-exp-item/NewExpItem'
 
 export default class StuEdicationalExperience extends React.Component<{}> {
   render() {
@@ -10,19 +11,7 @@ export default class StuEdicationalExperience extends React.Component<{}> {
         <FormTitle title={'教育经历'} />
         <div styleName="content">
           <ExpItem />
-          <div>
-            <div>
-              <div>
-                <span styleName="school-name">西南财经大学</span>
-                <span styleName="time-text">2011.9-2013.7</span>
-              </div>
-              <div styleName="degree-text">全日制本科</div>
-              <div style={{marginTop: '10px'}}>
-                <span styleName="major-text">外语学院/对外汉语专业</span>
-                <span styleName="edit-text" >编辑</span>
-              </div>
-            </div>
-          </div>
+          <NewExpItem />
         </div>
       </div>
     )
