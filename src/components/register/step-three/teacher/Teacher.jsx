384c5ab03c5f4e73a037eba39394621b521c01ac
wang <374951963@qq.com>
2018-01-12 11:24:20 +0800

    注册: 完成第三步中教师注册的基本信息界面

--- src/components/register/step-three/teacher/Teacher.jsx
+++ src/components/register/step-three/teacher/Teacher.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './teacher.css'
+import TchBaseInfo from './base-info/TchBaseInfo'
 
 export default class StepThreeTeacher extends React.Component<{}> {
   render() {
     return (
-      <div>teacher</div>
+      <div styleName="container">
+        <div styleName="title-wrapper">
+          <span styleName="title">step 3：完善详细信息</span>
+          <span styleName="next-step">|&nbsp;&nbsp;&nbsp;跳过此步骤</span>
+        </div>
+        <div styleName="form-container">
+          <TchBaseInfo />
+          <button styleName="confirm-button">确认</button>
+        </div>
+      </div>
     )
   }
 }
