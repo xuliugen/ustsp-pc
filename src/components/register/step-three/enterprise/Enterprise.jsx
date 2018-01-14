e917ac2b03e1e67e331dec82e12ced11a3492b5c
unknown <953847229@qq.com>
2018-01-15 00:31:49 +0800

    注册：第三步企业注册页面完成

--- src/components/register/step-three/enterprise/Enterprise.jsx
+++ src/components/register/step-three/enterprise/Enterprise.jsx
@@ -1,9 +1,25 @@
+// @flow
 import React from 'react'
+import './enterprise.css'
+import EnterBaseInfo from './base-info/EnterBaseInfo'
+import EnterOtherInfo from './other-info/EnterOtherInfo'
 
 export default class StepThreeEnterprise extends React.Component<{}> {
   render() {
     return (
-      <div>enterprise</div>
+      <div styleName="container" >
+        <div styleName="title-wrapper">
+          <span styleName="title">step 3：完善详细信息</span>
+          <span styleName="next-step">|&nbsp;&nbsp;&nbsp;跳过此步骤</span>
+        </div>
+        <div styleName="form-container">
+          <EnterBaseInfo />
+          <EnterOtherInfo />
+          <div styleName="confirm-btn">
+            <button>确认</button>
+          </div>
+        </div>
+      </div>
     )
   }
 }
---
+++ src/components/register/step-three/enterprise/other-info/EnterOtherInfo.jsx
@@ -0,0 +1,34 @@
+// @flow
+import React from 'react'
+import './enterOtherInfo.css'
+import { FormTitle } from '../../common'
+import { Form, Input } from 'antd'
+
+const FormItem = Form.Item
+const { TextArea } = Input
+
+export default class OtherInfo extends React.Component<{}> {
+  render() {
+    return (
+      <div styleName="other-info">
+        <FormTitle title="其他信息" />
+        <div styleName="content">
+          <Form layout="verticle" styleName="other-info-form">
+            <FormItem label="公司主页" >
+              <Input placeholder="公司主页" />
+            </FormItem>
+            <FormItem label="微信公众号" >
+              <Input placeholder="公司主页" />
+            </FormItem>
+            <FormItem label="公司简介" >
+              <div styleName="intro-container" >
+                <TextArea rows={8} style={{ overflowY: 'hidden' }} />
+                <span styleName="word-limit">字数限制: 0/400</span>
+              </div>
+            </FormItem>
+          </Form>
+        </div>
+      </div>
+    )
+  }
+}
