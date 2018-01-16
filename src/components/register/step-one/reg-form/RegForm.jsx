eb51de926919abc9c04b7d19eb8b71ce5d83bd2a
zero <zero@zerosrat.com>
2018-01-16 15:04:22 +0800

    注册：step1添加校验条件

--- src/components/register/step-one/reg-form/RegForm.jsx
+++ src/components/register/step-one/reg-form/RegForm.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Form, Select, Input, Icon, Checkbox, Button } from 'antd'
 import { observer, inject } from 'mobx-react'
+import { withRouter } from 'react-router-dom'
 import './regForm.css'
 
 const { Option } = Select
 const { Item: FormItem } = Form
 
+@withRouter
 @inject('registerStore')
 @observer
 class RegForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      verify: {
+        validateStatus: '' // success, error, validating
+      }
+    }
+  }
+
   componentDidMount() {
     this.props.form.resetFields()
     this.props.form.setFieldsValue(this.props.registerStore.initial)
@@ -23,14 +34,68 @@ class RegForm extends React.Component {
       console.log(err)
       this.props.registerStore.setInitialData(values)
     })
-    const { history } = this.props
-    history.push('/register/3')
+    // const { history } = this.props
+    // history.push('/register/3')
   }
 
   handleUserTypeChange = (val) => {
     const { setInitialData } = this.props.registerStore
     setInitialData({
       userType: val
+    })
+  }
+
+  handleValidateVerCode = (rule, value, callback) => {
+    if (this.state.verify.validateStatus === 'success') {
+      callback()
+    }
+    if (value) {
+      this.setState({
+        verify: { validateStatus: 'validating' }
+      })
+      setTimeout(() => {
+        const isMath = true
+        if (isMath) {
+          this.setState({
+            verify: { validateStatus: 'success' }
+          })
+          callback()
+        } else {
+          this.setState({
+            verify: { validateStatus: 'error' }
+          })
+          callback(new Error('验证码错误'))
+        }
+      }, 300)
+    }
+  }
+
+  handleValidatePwd = (rule, value, callback) => {
+    const { getFieldValue } = this.props.form
+    const confirmPwd = getFieldValue('confirmPwd')
+    if (confirmPwd) {
+      if (value && value !== confirmPwd) {
+        callback(new Error('两次输入不一致！'))
+      }
+    }
+    callback()
+  }
+
+  handleValidateConfirmPwd = (rule, value, callback) => {
+    const { getFieldValue } = this.props.form
+    if (value && value !== getFieldValue('pwd')) {
+      callback(new Error('两次输入不一致！'))
+    }
+    callback()
+  }
+
+  fetchVerCode = () => {
+    const { getFieldValue } = this.props.form
+    const tel = getFieldValue('userTel')
+    this.props.form.validateFields(['userTel'], (err) => {
+      if (!err) {
+        console.log('get code', tel)
+      }
     })
   }
 
@@ -51,7 +116,11 @@ class RegForm extends React.Component {
         </FormItem>
         <FormItem>
           {getFieldDecorator('userMail', {
-            rules: [{ required: true, message: '请输入邮箱' }]
+            validateTrigger: 'onBlur',
+            rules: [
+              { required: true, message: '请输入邮箱' },
+              { type: 'email', message: '邮箱格式有误' }
+            ]
           })(
             <Input
               size="large"
@@ -61,7 +130,11 @@ class RegForm extends React.Component {
         </FormItem>
         <FormItem>
           {getFieldDecorator('userTel', {
-            rules: [{ required: true, message: '请输入手机号' }]
+            validateTrigger: 'onBlur',
+            rules: [
+              { required: true, message: '请输入手机号' },
+              { pattern: /^1[0-9]{10}$/, message: '手机号格式有误' }
+            ]
           })(
             <Input
               size="large"
@@ -69,35 +142,53 @@ class RegForm extends React.Component {
               placeholder="手机号" />
           )}
         </FormItem>
-        <FormItem>
-          <div>
+        <div>
+          <FormItem
+            styleName="verifyIpt"
+            validateStatus={this.state.verify.validateStatus}
+            hasFeedback>
             {getFieldDecorator('userVer', {
-              rules: [{ required: true, message: '请输入验证码' }]
+              validateTrigger: 'onBlur',
+              rules: [
+                { required: true, message: '请输入验证码' },
+                { validator: this.handleValidateVerCode }
+              ]
             })(
               <Input
-                styleName="verifyIpt"
                 size="large"
                 prefix={<Icon type="check-circle-o" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                // validateStatus={this.state.verify.validateStatus}
                 placeholder="验证码" />
             )}
-            <Button styleName="verifyBtn" size="large" type="primary">获取</Button>
-          </div>
-        </FormItem>
+          </FormItem>
+          <Button styleName="verifyBtn" size="large" type="primary" onClick={this.fetchVerCode}>获取</Button>
+        </div>
         <FormItem>
-          {getFieldDecorator('userpwd1', {
-            rules: [{ required: true, message: '请输入密码' }]
+          {getFieldDecorator('pwd', {
+            validateTrigger: 'onBlur',
+            rules: [
+              { required: true, message: '请输入密码' },
+              { min: 6, message: '请输入大于等于6位的密码' },
+              { validator: this.handleValidatePwd }
+            ]
           })(
             <Input
+              type="password"
               size="large"
               prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
               placeholder="输入密码" />
           )}
         </FormItem>
         <FormItem>
-          {getFieldDecorator('userpwd2', {
-            rules: [{ required: true, message: '密码不一致' }]
+          {getFieldDecorator('confirmPwd', {
+            validateTrigger: 'onBlur',
+            rules: [
+              { required: true, message: '请确认密码' },
+              { validator: this.handleValidateConfirmPwd }
+            ]
           })(
             <Input
+              type="password"
               size="large"
               prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
               placeholder="确认密码" />
