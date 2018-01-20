0bd2018e63b77cd4f01dfa30c4658875c2a28595
unknown <953847229@qq.com>
2018-01-20 15:36:09 +0800

    注册：第三步学生添加教育经历功能

--- src/components/register/step-three/student/educational-experience/exp-item/ExpItem.jsx
+++ src/components/register/step-three/student/educational-experience/exp-item/ExpItem.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './expItem.css'
+import moment from 'moment'
 
 export default class NewExpItem extends React.Component<{}> {
   render() {
     return (
-      <div>
-        <div>
-          <span styleName="school-name">西南财经大学</span>
-          <span styleName="time-text">2011.9-2013.7</span>
+      <div styleName="stu-edu-experience">
+        <div styleName="row">
+          <span styleName="school-name">{this.props.exp.school}</span>
+          <span styleName="time-text">{moment(this.props.exp.startTime).format('YYYY-MM-DD')} - {moment(this.props.exp.endTime).format('YYYY-MM-DD')}</span>
         </div>
-        <div styleName="degree-text">全日制本科</div>
-        <div style={{ marginTop: '10px' }}>
-          <span styleName="major-text">外语学院/对外汉语专业</span>
+        <div styleName="degree-text row">{this.props.exp.level}</div>
+        <div styleName="row">
+          <span styleName="major-text">{this.props.exp.major}</span>
           <button styleName="edit-text" >编辑</button>
         </div>
       </div>
--- src/components/register/step-three/student/educational-experience/new-exp-item/NewExpItem.jsx
+++ src/components/register/step-three/student/educational-experience/new-exp-item/NewExpItem.jsx
@@ -1,45 +1,123 @@
 import React from 'react'
 import './newExpItem.css'
-import { Form, Input, Row, Col, DatePicker } from 'antd'
+import { Form, Input, Row, Col, DatePicker, Modal, Button, message } from 'antd'
+import { observer, inject } from 'mobx-react'
 
 const FormItem = Form.Item
 const MonthPicker = DatePicker.MonthPicker
 
-export default class ExpItem extends React.Component<{}> {
+@inject('registerStore')
+@observer
+class NewExpItem extends React.Component<{}> {
+  constructor() {
+    super()
+    this.state = {
+      loading: false
+    }
+  }
+
+  handleOk = (e) => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log(values)
+        const expItem = {
+          userId: this.props.registerStore.initial.uid,
+          school: values.school,
+          college: values.college,
+          major: values.major,
+          level: values.level,
+          startTime: values.date.valueOf(),
+          endTime: values.finishTime.valueOf()
+        }
+        this.props.confirmAdd(expItem)
+        this.props.closeModel()
+      } else {
+        message.error('请先完善必填信息')
+      }
+    })
+    // this.setState({
+    //   loading: true
+    // })
+  }
+
+  handleCancel = () => {
+    this.props.closeModel()
+  }
+
   render() {
+    const { getFieldDecorator } = this.props.form
     return (
       <div>
-        <Form layout="vertical" styleName="newEdu-form">
-          <FormItem label="学历级别">
-            <Input />
-          </FormItem>
-          <FormItem label="学校">
-            <Input />
-          </FormItem>
-          <FormItem label="学院">
-            <Input />
-          </FormItem>
-          <FormItem label="专业">
-            <Input />
-          </FormItem>
-          <Row gutter={20}>
-            <Col span={12} >
-              <FormItem label="入学时间">
-                <MonthPicker />
-              </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="毕业时间">
-                <MonthPicker />
-              </FormItem>
-            </Col>
-          </Row>
-        </Form>
-        <div>
-          <button styleName="store-button">保存</button>
-          <button styleName="cancel-button">取消</button>
-        </div>
+        <Modal
+          visible={this.props.visible}
+          title="教育经历"
+          destroyOnClose="true"
+          onOK={this.handleOK}
+          onCancel={this.handleCancel}
+          footer={[
+            <Button key="back" onClick={this.handleCancel} >取消</Button>,
+            <Button key="submit" type="primary" loading={this.state.loading} onClick={this.handleOk} >保存</Button>
+          ]}
+        >
+          <Form layout="vertical" styleName="newEdu-form">
+            <FormItem label="学历级别">
+              {getFieldDecorator('level', {
+                validateTrigger: 'onBlur',
+                rules: [{ required: true, message: '请输入学历级别' }]
+              })(
+                <Input />
+              )}
+            </FormItem>
+            <FormItem label="学校">
+              {getFieldDecorator('school', {
+                validateTrigger: 'onBlur',
+                rules: [{ required: true, message: '请输入就职学校' }]
+              })(
+                <Input />
+              )}
+            </FormItem>
+            <FormItem label="学院">
+              {getFieldDecorator('college', {
+                validateTrigger: 'onBlur',
+                rules: [{ required: true, message: '请输入学院' }]
+              })(
+                <Input />
+              )}
+            </FormItem>
+            <FormItem label="专业">
+              {getFieldDecorator('major', {
+                validateTrigger: 'onBlur',
+                rules: [{ required: true, message: '请输入就读专业' }]
+              })(
+                <Input />
+              )}
+            </FormItem>
+            <Row gutter={20}>
+              <Col span={12} >
+                <FormItem label="入学时间">
+                  {getFieldDecorator('date', {
+                    rules: [{ required: true, message: '请选择入学时间' }]
+                  })(
+                    <MonthPicker />
+                  )}
+                </FormItem>
+              </Col>
+              <Col span={12}>
+                <FormItem label="毕业时间">
+                  {getFieldDecorator('finishTime', {
+                    rules: [{ required: true, message: '请选择毕业时间' }]
+                  })(
+                    <MonthPicker />
+                  )}
+                </FormItem>
+              </Col>
+            </Row>
+          </Form>
+        </Modal>
       </div>
     )
   }
 }
+
+export default Form.create()(NewExpItem)
