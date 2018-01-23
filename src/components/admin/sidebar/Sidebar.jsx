4b3cb4e52446a99cbffde8ddfb37292c16835de7
zero <zero@zerosrat.com>
2018-01-23 16:43:50 +0800

    admin: 使用js配置的sidebar显示

--- src/components/admin/sidebar/Sidebar.jsx
+++ src/components/admin/sidebar/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import { Menu, Icon } from 'antd'
 import './sidebar.css'
+import { routes } from './routes'
 
 const SubMenu = Menu.SubMenu
 
@@ -14,6 +15,9 @@ export default class Sidebar extends React.Component {
   constructor() {
     super()
     this.handleItemSelect = this.handleItemSelect.bind(this)
+  }
+
+  componentWillMount() {
   }
 
   handleItemSelect({ key }) {
@@ -36,18 +40,22 @@ export default class Sidebar extends React.Component {
           defaultOpenKeys={['info']}
           mode="inline"
           inlineCollapsed={this.state.collapsed}
-          inlineIndent={30}
-          onSelect={this.handleItemSelect}>
-          <SubMenu key="info" title={<span><Icon type="user" /><span style={{ fontSize: '18px' }}>个人信息</span></span>}>
-            <Menu.Item key="info-detail" style={{ fontSize: '18px' }}>信息查看</Menu.Item>
-            <Menu.Item key="info-mod" style={{ fontSize: '18px' }}>修改信息</Menu.Item>
-            <Menu.Item key="info-security" style={{ fontSize: '18px' }}>修改密码</Menu.Item>
-          </SubMenu>
-          <SubMenu key="demand" title={<span><Icon type="check-square-o" /><span style={{ fontSize: '18px' }}>需求对接</span></span>}>
-            <Menu.Item key="demand-new" style={{ fontSize: '18px' }}>发起新需求</Menu.Item>
-            <Menu.Item key="demand-publish" style={{ fontSize: '18px' }}>已发布的需求</Menu.Item>
-            <Menu.Item key="demand-process" style={{ fontSize: '18px' }}>已报名/已承接的需求</Menu.Item>
-          </SubMenu>
+          inlineIndent={30}>
+          {/* // onSelect={this.handleItemSelect}> */}
+          {routes.map(({key, title, children}) => {
+            const SubMenuIcon = title.icon ? <Icon type="check-square-o" /> : null
+            const MenuItems = children.map(({key, to, text}) => {
+              const ItemLink = to ? <Link to={to}>{text}</Link> : <span>{text}</span>
+              return (
+                <Menu.Item key={key} style={{ fontSize: '18px' }}>{ItemLink}</Menu.Item>
+              )
+            })
+            return (
+              <SubMenu key={key} title={<span>{SubMenuIcon}<span style={{ fontSize: '18px' }}>{title.text}</span></span>}>
+                {MenuItems}
+              </SubMenu>
+            )
+          })}
         </Menu>
       </section>
     )
---
+++ src/components/admin/sidebar/routes.js
@@ -0,0 +1,45 @@
+export const routes = [
+  {
+    key: 'info',
+    title: {
+      icon: 'user',
+      text: '个人信息'
+    },
+    children: [
+      {
+        key: 'info-detail',
+        to: '',
+        text: '信息查看'
+      }, {
+        key: 'info-mod',
+        to: '',
+        text: '修改信息'
+      }, {
+        key: 'info-security',
+        to: '',
+        text: '修改密码'
+      }
+    ]
+  }, {
+    key: 'demand',
+    title: {
+      icon: 'check-square-o',
+      text: '个人信息'
+    },
+    children: [
+      {
+        key: 'demand-new',
+        to: '',
+        text: '发布新需求'
+      }, {
+        key: 'demand-mod',
+        to: '',
+        text: '已发布的需求'
+      }, {
+        key: 'demand-security',
+        to: '',
+        text: '已报名/已承接的需求'
+      }
+    ]
+  }
+]
