af74af856f84cf152e0d97dc12aeb7c67870284a
wang <374951963@qq.com>
2018-01-23 17:35:31 +0800

    需求: 完成已发布需求的已关注模块界面

--- src/components/admin/demand/detail/DemandDetail.jsx
+++ src/components/admin/demand/detail/DemandDetail.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import './demandDetail.css'
+import RegisteredPerson from './registered-person/RegisteredPerson'
 
 export default class DemandDetail extends React.Component {
   render() {
     return (
-      <div>dd</div>
+      <div>
+        <div styleName="title">
+          <span styleName="title-text">报名/关注详情</span>
+          <button styleName="title-button">查看全部</button>
+        </div>
+        <div styleName="persons">
+          <div styleName="registered" >
+            <div styleName="registered-title">已报名</div>
+            <div styleName="persons-form">
+              <RegisteredPerson />
+              <RegisteredPerson />
+            </div>
+          </div>
+          <div styleName="followed">s</div>
+        </div>
+      </div>
     )
   }
 }
---
+++ src/components/admin/demand/detail/registered-person/RegisteredPerson.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './registeredPerson.css'
+import { Avatar } from 'antd'
+
+export default class RegisteredPerson extends React.Component {
+  render() {
+    return (
+      <div styleName="person-item">
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+          <Avatar
+            // src=''
+            icon="user" />
+          <span styleName="name">钟爽</span>
+          <Avatar icon="man" size="small" styleName="render">男</Avatar>
+
+        </div>
+        <span styleName="person-info">学生 / 西南财经大学 / email@163.com </span>
+        <span styleName="register-time">报名时间：2014-10-24 19:20  </span>
+      </div>
+    )
+  }
+}
