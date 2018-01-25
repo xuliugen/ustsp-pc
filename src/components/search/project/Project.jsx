9cece951fafa6440d2715fb6444dc41428c547d0
zero <zero@zerosrat.com>
2018-01-25 13:04:53 +0800

    搜索：初始化

---
+++ src/components/search/project/Project.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class SearchProject extends React.Component {
+  render() {
+    return (
+      <div>pro</div>
+    )
+  }
+}
---
+++ src/views/search/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchProject from 'components/search/project/Project'
+
+export default class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchProject />
+      </div>
+    )
+  }
+}
