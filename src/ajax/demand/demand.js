a421f8525ee09f2a336c2cb532b94ce0eceefb08
wang <374951963@qq.com>
2018-01-26 11:04:08 +0800

    修改已关注者的接口字段

--- src/ajax/demand/demand.js
+++ src/ajax/demand/demand.js
@@ -7,9 +7,9 @@ export default {
 
   getPublishedDemand(userId, page, row, status) {
     if (status === undefined) {
-      return ajax.get(`/project/query/status?userId=${userId}&page=${page}&rows=${row}`)
+      return ajax.get(`/project/query/status?userid=${userId}&page=${page}&rows=${row}`)
     }
-    return ajax.get(`/project/query/status?userId=${userId}&status=${status}&page=${page}&rows=${row}`)
+    return ajax.get(`/project/query/status?userid=${userId}&status=${status}&page=${page}&rows=${row}`)
   },
 
   getDemandApplicants(userId, projectId) {
