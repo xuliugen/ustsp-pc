23747e60a5baefdde1af437c98c1bd8003e7b1f8
CandiceXiao <candicexiao13@gmail.com>
2018-01-27 12:50:15 +0800

    【detail/teacher】modify some stylename

--- src/views/detail/teacher/Teacher.jsx
+++ src/views/detail/teacher/Teacher.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 import { Header, Footer } from 'components/common'
 import { Divider } from 'antd'
 import './teacher.css'
-import Nav from 'components/detail/teacher/nav/Nav'
 import InfoTeacher from 'components/detail/teacher/info-teacher/InfoTeacher'
-import Projects from 'components/detail/teacher/projects/Projects'
-import Introduction from 'components/detail/teacher/introduction/Introduction'
-import Educations from 'components/detail/teacher/educations/Educations'
-import Awards from 'components/detail/teacher/awards/Awards'
-import Experiences from 'components/detail/teacher/experiences/Experiences'
 
-import { TalentsRmd, CommonFriends, Others } from 'components/detail/common'
+import { TalentsRmd, CommonFriends, Others, Nav, ProjectsJoin, Introduction, Educations, Awards, Experiences } from 'components/detail/common'
 
 export default class Teacher extends React.Component {
   render() {
@@ -26,7 +20,7 @@ export default class Teacher extends React.Component {
           <div styleName="left-container">
             <InfoTeacher />
             <Divider><span styleName="divider">TA参与的平台项目</span></Divider>
-            <Projects />
+            <ProjectsJoin />
             <Divider><span styleName="divider">个人简介</span></Divider>
             <Introduction />
             <Divider><span styleName="divider">教育经历</span></Divider>
