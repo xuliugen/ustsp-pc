6650f60af72f61aaad7b13b470517e1d9b4082f2
wang <374951963@qq.com>
2018-01-30 19:14:29 +0800

    完成完善信息界面时间显示不对问题

--- src/components/register/step-three/teacher/research/research-item/ResearchItem.jsx
+++ src/components/register/step-three/teacher/research/research-item/ResearchItem.jsx
@@ -9,12 +9,13 @@ export default class ResearchItem extends React.Component<{}> {
       <div styleName="research-item">
         <div>
           <span styleName="research-name">{this.props.rea.projectName}</span>
-          <span styleName="time-text">{moment(this.props.rea.startTime).format('YYYY.M')} - {moment(this.props.rea.endTime).format('YYYY.M')}</span>
+          {(this.props.rea.startTime && this.props.rea.endTime) &&
+          <span styleName="time-text">{moment(this.props.rea.startTime).format('YYYY.M')} - {moment(this.props.rea.endTime).format('YYYY.M')}</span>}
         </div>
         <div styleName="ul-container">
           <ul style={{ paddingLeft: '18px' }}>
-            <li styleName="li-text">{this.props.rea.projectLevel}</li>
-            <li styleName="li-text">经费 ¥  {this.props.rea.funding}</li>
+            {(this.props.rea.projectLevel) && <li styleName="li-text">级别: {this.props.rea.projectLevel}</li>}
+            <li styleName="li-text">经费:   {this.props.rea.funding ? '¥' + this.props.rea.funding : '暂无'}</li>
           </ul>
           <div styleName="edit-container">
             <Icon type="edit" styleName="edit-incon" />
