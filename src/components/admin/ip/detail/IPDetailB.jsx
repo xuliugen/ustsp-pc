588cc6607636628613750d4ebe8c9e062f21f8d7
zeroyu <zero@zerosrat.com>
2018-03-29 19:04:38 +0800

    ip: card组件封装

--- src/components/admin/ip/detail/IPDetailB.jsx
+++ src/components/admin/ip/detail/IPDetailB.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './ipDetail.css'
+
+import DetailInfo from './detail-info/DetailInfo'
+import EnquiryCardB from './enquiry-card-b/EnquiryCardB'
 
 export default class IPDetailB extends React.Component {
   render() {
     return (
-      <div>b</div>
+      <div>
+        <DetailInfo />
+        <div styleName="card-wrapper">
+          <EnquiryCardB />
+        </div>
+      </div>
     )
   }
 }
---
+++ src/components/admin/ip/detail/enquiry-card-b/EnquiryCardB.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../common/card/Card'
+
+export default class EnquiryCardB extends React.Component {
+  render() {
+    return (
+      <Card title="询价 & 购买">
+        123
+      </Card>
+    )
+  }
+}
