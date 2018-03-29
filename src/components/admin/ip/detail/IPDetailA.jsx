1879dbb3f9ef28097ea4b27eaa6680107e568c2b
unknown <953847229@qq.com>
2018-03-29 11:37:49 +0800

    ip：甲方签订合同组件界面完成

--- src/components/admin/ip/detail/IPDetailA.jsx
+++ src/components/admin/ip/detail/IPDetailA.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import DetailInfo from './detail-info/DetailInfo'
+import SignCardA from './sign-card-a/SignCardA'
 
 export default class IPDetailA extends React.Component {
   render() {
     return (
       <div>
         <DetailInfo />
+        <div style={{ marginTop: '30px' }}>
+          <SignCardA />
+        </div>
       </div>
     )
   }
---
+++ src/components/admin/ip/detail/sign-card-a/SignCardA.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Icon, Avatar } from 'antd'
+import './signCardA.css'
+import ImgAvatar from 'src/assets/defaultAvatar.svg'
+
+export default class SignCardA extends React.Component {
+  render() {
+    return (
+      <div styleName="content">
+        <div styleName="message">
+          <Icon type="check-circle" style={{ color: '#62f326', fontSize: '30px' }} />
+          <span style={{ marginLeft: '20px' }} >签订合同请求已经发送，等待购买者确认。</span>
+        </div>
+        <div styleName="partyb">
+          <div styleName="title">
+            受让方信息
+          </div>
+          <div styleName="partyb-info">
+            <div styleName="partyb-avatar">
+              <Avatar src={ImgAvatar} icon="user" />
+              <span>&nbsp;&nbsp;吴彦祖</span>
+            </div>
+            <span>教授/中央财经大学-本科</span>
+            <span>签订发起时间：2018-01-10 16:30:56</span>
+            <span>转让金额：玖拾玖万玖仟玖佰玖拾整</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
