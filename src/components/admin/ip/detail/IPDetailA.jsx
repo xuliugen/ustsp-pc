74c5e866a71b9809dabdc7ad656c40f657560495
zeroyu <zero@zerosrat.com>
2018-03-29 11:06:56 +0800

    ip:甲方询价初始化，询价人组件初始化

--- src/components/admin/ip/detail/IPDetailA.jsx
+++ src/components/admin/ip/detail/IPDetailA.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import EnquiryCardA from './enquiry-card-a/EnquiryCardA'
 
 export default class IPDetailA extends React.Component {
   render() {
     return (
-      <div>a</div>
+      <div>
+        <EnquiryCardA />
+      </div>
     )
   }
 }
---
+++ src/components/admin/ip/detail/enquiry-card-a/EnquiryCardA.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './enquiryCardA.css'
+import EnquiryPerson from './enquiry-person/EnquiryPerson'
+
+export default class EnquiryCardA extends React.Component {
+  render() {
+    return (
+      <div styleName="root">
+        <div styleName="title">
+          询价情况 & 购买意愿
+        </div>
+        <div styleName="content">
+          <div styleName="enquiry">
+            <div styleName="persons-title">询价(10)</div>
+            <EnquiryPerson />
+          </div>
+          <div styleName="send">
+            <div styleName="persons-title">已发送文件(10)</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
