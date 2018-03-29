09341dae3e5167137f4e1f8396b821e68b6484a6
konghao19970906 <495243922@qq.com>
2018-03-29 16:19:16 +0800

    'IP：js代码修改'

--- src/components/admin/ip/detail/IPTransferInfo/IPTransferInfoCardA.jsx
+++ src/components/admin/ip/detail/IPTransferInfo/IPTransferInfoCardA.jsx
@@ -18,26 +18,26 @@ export default class IPTransferInfoA extends React.Component {
   }
   priceDigitUpper(n) {
     n = n * 10000
-    var fraction = ['角', '分']
-    var digit = [
+    const fraction = ['角', '分']
+    const digit = [
       '零', '壹', '贰', '叁', '肆',
       '伍', '陆', '柒', '捌', '玖'
     ]
-    var unit = [
+    const unit = [
       ['元', '万', '亿'],
       ['', '拾', '佰', '仟']
     ]
-    var head = n < 0 ? '欠' : ''
+    const head = n < 0 ? '欠' : ''
     n = Math.abs(n)
-    var s = ''
-    for (var i = 0; i < fraction.length; i++) {
+    let s = ''
+    for (let i = 0; i < fraction.length; i++) {
       s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '')
     }
     s = s || '整'
     n = Math.floor(n)
-    for (i = 0; i < unit[0].length && n > 0; i++) {
-      var p = ''
-      for (var j = 0; j < unit[1].length && n > 0; j++) {
+    for (let i = 0; i < unit[0].length && n > 0; i++) {
+      let p = ''
+      for (let j = 0; j < unit[1].length && n > 0; j++) {
         p = digit[n % 10] + unit[1][j] + p
         n = Math.floor(n / 10)
       }
@@ -48,7 +48,7 @@ export default class IPTransferInfoA extends React.Component {
       .replace(/^整$/, '零元整')
   }
 
-  componentWillMount() {
+  componentDidMount() {
     // 请求相应信息
   }
   render() {
