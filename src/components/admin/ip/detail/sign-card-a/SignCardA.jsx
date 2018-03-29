c9ec0c14fd62f92d8baf168dd72db3c35c2205f2
unknown <953847229@qq.com>
2018-03-29 14:45:18 +0800

    ip：甲方签订合同组件修改

--- src/components/admin/ip/detail/sign-card-a/SignCardA.jsx
+++ src/components/admin/ip/detail/sign-card-a/SignCardA.jsx
@@ -6,23 +6,26 @@ import ImgAvatar from 'src/assets/defaultAvatar.svg'
 export default class SignCardA extends React.Component {
   render() {
     return (
-      <div styleName="content">
-        <div styleName="message">
-          <Icon type="check-circle" style={{ color: '#62f326', fontSize: '30px' }} />
-          <span style={{ marginLeft: '20px' }} >签订合同请求已经发送，等待购买者确认。</span>
-        </div>
-        <div styleName="partyb">
-          <div styleName="title">
-            受让方信息
+      <div>
+        <div styleName="title">签订合同</div>
+        <div styleName="content">
+          <div styleName="message">
+            <Icon type="check-circle" style={{ color: '#62f326', fontSize: '30px' }} />
+            <span style={{ marginLeft: '20px' }} >签订合同请求已经发送，等待购买者确认。</span>
           </div>
-          <div styleName="partyb-info">
-            <div styleName="partyb-avatar">
-              <Avatar src={ImgAvatar} icon="user" />
-              <span>&nbsp;&nbsp;吴彦祖</span>
+          <div styleName="partyb">
+            <div styleName="info-title">
+              受让方信息
             </div>
-            <span>教授/中央财经大学-本科</span>
-            <span>签订发起时间：2018-01-10 16:30:56</span>
-            <span>转让金额：玖拾玖万玖仟玖佰玖拾整</span>
+            <div styleName="partyb-info">
+              <div styleName="partyb-avatar">
+                <Avatar src={ImgAvatar} icon="user" />
+                <span>&nbsp;&nbsp;吴彦祖</span>
+              </div>
+              <span>教授/中央财经大学-本科</span>
+              <span>签订发起时间：2018-01-10 16:30:56</span>
+              <span>转让金额：玖拾玖万玖仟玖佰玖拾整</span>
+            </div>
           </div>
         </div>
       </div>
