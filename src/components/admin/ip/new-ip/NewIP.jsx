2161f490ef0d1acbadd650c4e20a0465a2c57694
zeroyu <zero@zerosrat.com>
2018-03-30 11:13:38 +0800

    ip: 发专利添加校验

--- src/components/admin/ip/new-ip/NewIP.jsx
+++ src/components/admin/ip/new-ip/NewIP.jsx
@@ -7,7 +7,26 @@ const Option = Select.Option
 const { TextArea } = Input
 const Dragger = Upload.Dragger
 
+@Form.create()
 export default class NewIP extends React.Component {
+  submitForm = () => {
+    this.props.form.validateFields((err, values) => {
+      if (err) {
+        message.error('请完善信息')
+      } else {
+        console.log(values)
+      }
+    })
+  }
+
+  normFile = (e) => {
+    console.log('Upload event:', e)
+    if (Array.isArray(e)) {
+      return e
+    }
+    return e && e.fileList
+  }
+
   render() {
     const uploadProps = {
       name: 'file',
@@ -25,6 +44,7 @@ export default class NewIP extends React.Component {
         }
       }
     }
+    const { getFieldDecorator } = this.props.form
     return (
       <div styleName="root">
         <div styleName="title">
@@ -35,148 +55,266 @@ export default class NewIP extends React.Component {
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="专利名称">
-                  <Input />
+                  {getFieldDecorator('patentName', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="专利申请号">
-                  <Input />
+                  {getFieldDecorator('applicationNumber', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
               <Col span={12}>
                 <FormItem label="申请日">
-                  <DatePicker style={{ width: '100%' }} />
+                  {getFieldDecorator('applicationDate', {
+                    rules: [
+                      { required: true, message: '请选择时间' }
+                    ]
+                  })(
+                    <DatePicker style={{ width: '100%' }} />
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="专利类型">
-                  <Select style={{ width: '100%' }}>
-                    <Option value="发明">发明</Option>
-                    <Option value="实用新型">实用新型</Option>
-                    <Option value="外观设计">外观设计</Option>
-                    <Option value="其他">其他</Option>
-                  </Select>
+                  {/* to mod */}
+                  {getFieldDecorator('patentType', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请选择' }
+                    ]
+                  })(
+                    <Select style={{ width: '100%' }}>
+                      <Option value="发明">发明</Option>
+                      <Option value="实用新型">实用新型</Option>
+                      <Option value="外观设计">外观设计</Option>
+                      <Option value="其他">其他</Option>
+                    </Select>
+                  )}
                 </FormItem>
               </Col>
               <Col span={12}>
                 <FormItem label="行业分类">
-                  <Select style={{ width: '100%' }}>
-                    <Option value="A：人类生活需要">A：人类生活需要</Option>
-                    <Option value="B：作业运输">B：作业运输</Option>
-                    <Option value="C：化学，冶金">C：化学，冶金</Option>
-                    <Option value="D：纺织和造纸">D：纺织和造纸</Option>
-                    <Option value="E：固定构造">E：固定构造</Option>
-                    <Option value="F：机械工程，照明，加热，武器，爆破">F：机械工程，照明，加热，武器，爆破</Option>
-                    <Option value="G：物理">G：物理</Option>
-                    <Option value="H：电学">H：电学</Option>
-                    <Option value="其他">其他</Option>
-                  </Select>
+                  {/* to mod */}
+                  {getFieldDecorator('industryCategory', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请选择' }
+                    ]
+                  })(
+                    <Select style={{ width: '100%' }}>
+                      <Option value="A：人类生活需要">A：人类生活需要</Option>
+                      <Option value="B：作业运输">B：作业运输</Option>
+                      <Option value="C：化学，冶金">C：化学，冶金</Option>
+                      <Option value="D：纺织和造纸">D：纺织和造纸</Option>
+                      <Option value="E：固定构造">E：固定构造</Option>
+                      <Option value="F：机械工程，照明，加热，武器，爆破">F：机械工程，照明，加热，武器，爆破</Option>
+                      <Option value="G：物理">G：物理</Option>
+                      <Option value="H：电学">H：电学</Option>
+                      <Option value="其他">其他</Option>
+                    </Select>
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="法律状态">
-                  <Select style={{ width: '100%' }}>
-                    <Option value="等年登印费">等年登印费</Option>
-                    <Option value="等待恢复">等待恢复</Option>
-                    <Option value="专利权维持">专利权维持</Option>
-                    <Option value="等年费滞纳金">等年费滞纳金</Option>
-                    <Option value="权利终止，等恢复">权利终止，等恢复</Option>
-                    <Option value="权利失效">权利失效</Option>
-                  </Select>
+                  {getFieldDecorator('legalStatus', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请选择' }
+                    ]
+                  })(
+                    <Select style={{ width: '100%' }}>
+                      <Option value="等年登印费">等年登印费</Option>
+                      <Option value="等待恢复">等待恢复</Option>
+                      <Option value="专利权维持">专利权维持</Option>
+                      <Option value="等年费滞纳金">等年费滞纳金</Option>
+                      <Option value="权利终止，等恢复">权利终止，等恢复</Option>
+                      <Option value="权利失效">权利失效</Option>
+                    </Select>
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="公开号">
-                  <Input />
+                  {getFieldDecorator('publicationNumber', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
               <Col span={12}>
                 <FormItem label="公开日">
-                  <DatePicker style={{ width: '100%' }} />
+                  {getFieldDecorator('publicationDate', {
+                    rules: [
+                      { required: true, message: '请选择时间' }
+                    ]
+                  })(
+                    <DatePicker style={{ width: '100%' }} />
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="专利权人">
-                  <Input />
+                  {getFieldDecorator('applicant', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="发明人">
-                  <Input />
+                  {getFieldDecorator('inventor', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="主分类号">
-                  <Input />
+                  {getFieldDecorator('classificationNumber', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
               <Col span={12}>
                 <FormItem label="分类号">
-                  <Input />
+                  {getFieldDecorator('classNumber', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row gutter={20}>
               <Col span={12}>
                 <FormItem label="地址">
-                  <Input />
+                  {getFieldDecorator('address', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
               <Col span={12}>
                 <FormItem label="国省代码">
-                  <Input />
+                  {getFieldDecorator('provinceCode', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <Input />
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row>
               <Col span={24}>
                 <FormItem label="摘要">
-                  <TextArea rows={8} />
+                  {getFieldDecorator('abstracts', {
+                    validateTrigger: 'onBlur',
+                    rules: [
+                      { required: true, message: '请填写' }
+                    ]
+                  })(
+                    <TextArea rows={8} />
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row>
               <Col span={24}>
                 <FormItem label="PDF全文">
-                  <Dragger {...uploadProps}>
-                    <p className="ant-upload-drag-icon">
-                      <Icon type="inbox" />
-                    </p>
-                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                    <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
-                  </Dragger>
+                  {getFieldDecorator('document', {
+                    valuePropName: 'fileList',
+                    getValueFromEvent: this.normFile,
+                    rules: [
+                      { required: true, message: '请上传' }
+                    ]
+                  })(
+                    <Dragger {...uploadProps}>
+                      <p className="ant-upload-drag-icon">
+                        <Icon type="inbox" />
+                      </p>
+                      <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                      <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
+                    </Dragger>
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <Row>
               <Col span={24}>
                 <FormItem label="评估文件">
-                  <Dragger {...uploadProps}>
-                    <p className="ant-upload-drag-icon">
-                      <Icon type="inbox" />
-                    </p>
-                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                    <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
-                  </Dragger>
+                  {getFieldDecorator('appraisalDocument', {
+                    valuePropName: 'fileList',
+                    getValueFromEvent: this.normFile
+                  })(
+                    <Dragger {...uploadProps}>
+                      <p className="ant-upload-drag-icon">
+                        <Icon type="inbox" />
+                      </p>
+                      <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                      <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
+                    </Dragger>
+                  )}
                 </FormItem>
               </Col>
             </Row>
             <div styleName="pubBtn-wrapper">
-              <Button type="primary" size="large">发布</Button>
+              <Button type="primary" size="large" onClick={this.submitForm}>发布</Button>
             </div>
           </Form>
         </div>
