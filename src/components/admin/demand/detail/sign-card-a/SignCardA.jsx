93413fbfc30789f1e9ad990900d1177fa191ea13
unknown <953847229@qq.com>
2018-04-12 11:36:17 +0800

    需求：甲方撤销签单逻辑

--- src/components/admin/demand/detail/sign-card-a/SignCardA.jsx
+++ src/components/admin/demand/detail/sign-card-a/SignCardA.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Icon, Avatar } from 'antd'
+import { Icon, Avatar, message, Modal } from 'antd'
 import { observer, inject } from 'mobx-react'
 import moment from 'moment'
 import { withRouter } from 'react-router-dom'
 import './signCardA.css'
+import { DemandApi } from 'src/ajax'
+
+const confirm = Modal.confirm
 
 @withRouter
-@inject('demandStore')
+@inject('demandStore', 'userStore')
 @observer
 export default class SignCardA extends React.Component {
   showUserType(partyB) {
@@ -31,6 +34,28 @@ export default class SignCardA extends React.Component {
       type = 'company'
     }
     this.props.history.push(`/${type}/${person.partyId}`)
+  }
+
+  handleCancel(partyB) {
+    confirm({
+      title: '确定要撤销签单请求吗？',
+      content: '可以尝试刷新页面以获取最新状态',
+      onOk: async () => {
+        try {
+          await DemandApi.changeDemandStatus({
+            partyId: partyB.partyId,
+            ownerId: this.props.userStore.user.id,
+            projectId: this.props.match.params.id,
+            status: 'applying'
+          })
+          message.success('撤销签单成功')
+          // 刷新项目信息
+          this.props.demandStore.dispatchGetDemandInfo()
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    })
   }
 
   render() {
@@ -59,8 +84,8 @@ export default class SignCardA extends React.Component {
             </div>
           </div>
           <div styleName="cancel">
-            <p style={{marginBottom: '0.5em'}}>若对方长时间未确认，您可以申请撤回签单时间，项目回到报名状态继续报名</p>
-            <a>申请撤销</a>
+            <p style={{ marginBottom: '0.5em' }}>若对方长时间未确认，您可以申请撤回签单时间，项目回到报名状态继续报名</p>
+            <a onClick={this.handleCancel.bind(this, partyB)}>申请撤销</a>
           </div>
         </div>
       </div>
