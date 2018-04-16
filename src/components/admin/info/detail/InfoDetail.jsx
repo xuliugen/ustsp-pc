ff63423e19eed2339a9d31d7c5a121884545531b
konghao19970906 <495243922@qq.com>
2018-04-16 15:16:22 +0800

    '信息展示小改动'

--- src/components/admin/info/detail/InfoDetail.jsx
+++ src/components/admin/info/detail/InfoDetail.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { inject } from 'mobx-react'
-import StuInfo from './info-student/StuInfo'
+import { inject, observer } from 'mobx-react'
+// import StuInfo from './info-student/StuInfo'
 import TeacherInfo from './info-teacher/TeacherInfo'
 import EnterpriseInfo from './info-enterprise/EnterpriseInfo'
 
+@observer
 @inject('userStore')
 export default class InfoDetail extends React.Component {
   render() {
     const { user } = this.props.userStore
-    let { InfoModule } = {}
+    let InfoModule
     switch (user.userType) {
-      case 1 : InfoModule = StuInfo
+      case 1 : InfoModule = EnterpriseInfo
         break
       case 2 : InfoModule = TeacherInfo
         break
