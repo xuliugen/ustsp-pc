51e948fbcedcc4e25a4059989fa2bcd1804520c5
letch <953847229@qq.com>
2018-04-18 11:06:17 +0800

    消息：系统消息界面

--- src/components/admin/message/system-msg/SystemMsg.jsx
+++ src/components/admin/message/system-msg/SystemMsg.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import MsgItem from './msg-item/MsgItem'
+import './systemMsg.css'
 
 export default class StystemMsg extends React.Component {
+  state = {
+    mgnt: false
+  }
+
+  handleMgnt(status) {
+    this.setState({
+      mgnt: status
+    })
+  }
+
   render() {
     return (
-      <div>system msg</div>
+      <div styleName="msg-wrapper">
+        <div styleName="title">
+          <span>系统消息</span>
+          {this.state.mgnt ? (
+            <div>
+              <span styleName="tags" >全选</span>
+              <span styleName="tags" style={{ margin: '0px 20px' }}>删除</span>
+              <span styleName="tags" onClick={this.handleMgnt.bind(this, false)}>退出</span>
+            </div>
+          ) : (
+            <span styleName="tags" onClick={this.handleMgnt.bind(this, true)}>管理</span>
+          )}
+        </div>
+        <div styleName="content">
+          <MsgItem mgnt={this.state.mgnt} />
+          <MsgItem mgnt={this.state.mgnt} />
+          <MsgItem mgnt={this.state.mgnt} />
+        </div>
+      </div>
     )
   }
 }
