c449cd45110f2ea380c2b6342e7bf1690f38b95d
letch <953847229@qq.com>
2018-04-19 15:51:52 +0800

    消息：项目动态界面

--- src/components/admin/message/demand-news/news-item/NewsITem.jsx
+++ src/components/admin/message/demand-news/news-item/NewsITem.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ImgSystem from 'src/assets/systemIcon.png'
-import './msgItem.css'
+import './newsItem.css'
 
-export default class MsgItem extends React.Component {
+export default class NewsItem extends React.Component {
   render() {
     return (
       <div styleName="root">
         <img src={ImgSystem} styleName="avatar" />
-        <div styleName="msg-info">
+        <div styleName="news-info">
           <div>
-            <span>UppFind管理员</span>
+            <span>基于Ffmpeg的直播推流系统</span>
             <span styleName="time">2018-01-24 19:20</span>
           </div>
           <div styleName="content">
-            尊敬的用户15682032376您好，感谢您注册使用UppFind！
+            项目有了新进度，请注意查看！
           </div>
         </div>
       </div>
