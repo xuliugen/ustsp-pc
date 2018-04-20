33272af296d0a5325fd4dcffe0316a7138c86c4e
letch <953847229@qq.com>
2018-04-20 16:28:04 +0800

    消息：修改获取消息备注

--- src/ajax/message/message.js
+++ src/ajax/message/message.js
@@ -1,7 +1,7 @@
 import ajax from 'src/ajax'
 
 export default {
-  // queryType 1.系统消息 2.好友消息 3.项目消息 4.专利消息
+  // queryType 1系统消息，11好友消息，21甲传给乙的项目消息，22乙传给甲的项目消息， 31专利消息'
   fetchMessages(userId, queryType, page, rows) {
     return ajax.get(`/message/query/messages?userId=${userId}&queryType=${queryType}&page=${page}&rows=${rows}`)
   },
