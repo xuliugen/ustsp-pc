420c2825e3f3f0a847de859913110ab4ce75d466
zero <zero@zerosrat.com>
2018-04-21 14:50:44 +0800

    fix: sidebar 的选中随路由变化而变化

--- src/components/admin/sidebar/Sidebar.jsx
+++ src/components/admin/sidebar/Sidebar.jsx
@@ -14,21 +14,18 @@ export default class Sidebar extends React.Component {
     selectedKeys: []
   }
 
-  // constructor() {
-  //   super()
-  //   this.handleItemSelect = this.handleItemSelect.bind(this)
-  // }
-
-  componentWillMount() {
-    const { pathname } = this.props.history.location
+  setKeys(pathname) {
     const route = pathname.split('/admin/')[1]
     if (!route) return
     const routeModule = route.split('/')[0]
     routes.forEach(({ key, children }) => {
       if (key === routeModule) {
-        this.setState((pre) => ({
-          openKeys: pre.openKeys.concat(key)
-        }))
+        // this.setState((pre) => ({
+        //   openKeys: pre.openKeys.concat(key)
+        // }))
+        this.setState({
+          openKeys: [key]
+        })
       }
       const routeChild = route.split('/')[1]
       if (routeChild) {
@@ -43,24 +40,39 @@ export default class Sidebar extends React.Component {
     })
   }
 
-  // handleItemSelect({ key }) {
-  //   const { history } = this.props
-  //   switch (key) {
-  //     case 'demand-new':
-  //       history.push('/admin/demand/new-demand')
-  //       break
-  //     default:
-  //       break
-  //   }
-  // }
+  handleSubMenuClick = ({key}) => {
+    this.setState(pre => {
+      if (pre.openKeys.includes(key)) {
+        let openKeys = pre.openKeys
+        openKeys.splice(pre.openKeys.findIndex(e => e === key), 1)
+        return {
+          openKeys: openKeys
+        }
+      } else {
+        return {
+          openKeys: pre.openKeys.concat(key)
+        }
+      }
+    })
+  }
+
+  componentWillMount() {
+    const { pathname } = this.props.history.location
+    this.setKeys(pathname)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { pathname } = nextProps.history.location
+    this.setKeys(pathname)
+  }
 
   render() {
     return (
       <section styleName="sidebar-inner">
         <Menu
           styleName="sidebar"
-          defaultSelectedKeys={this.state.selectedKeys}
-          defaultOpenKeys={this.state.openKeys}
+          openKeys={this.state.openKeys}
+          selectedKeys={this.state.selectedKeys}
           mode="inline"
           inlineCollapsed={this.state.collapsed}
           inlineIndent={30}>
@@ -74,7 +86,10 @@ export default class Sidebar extends React.Component {
               )
             })
             return (
-              <SubMenu key={key} title={<span>{SubMenuIcon}<span style={{ fontSize: '18px' }}>{title.text}</span></span>}>
+              <SubMenu
+                key={key}
+                title={<span>{SubMenuIcon}<span style={{ fontSize: '18px' }}>{title.text}</span></span>}
+                onTitleClick={this.handleSubMenuClick}>
                 {MenuItems}
               </SubMenu>
             )
