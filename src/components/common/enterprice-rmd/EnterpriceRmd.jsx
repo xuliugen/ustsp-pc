e8335f1e9837b757bb35efbd69c320f29e2293f9
topgunlcs98 <liuchangshu98@163.com>
2018-05-29 13:10:25 +0800

    相关企业推荐-修改

--- src/components/common/enterprice-rmd/EnterpriceRmd.jsx
+++ src/components/common/enterprice-rmd/EnterpriceRmd.jsx
@@ -2,15 +2,27 @@ import React from 'react'
 import Header from 'components/detail/common/header/Header'
 import EnterpriceItem from './enterprice-item/EnterpriceItem'
 import './enterpriceRmd.css'
+import { EtpInfoApi } from 'src/ajax'
+import { withRouter } from 'react-router-dom'
+
+@withRouter
 export default class EnterpriceRmd extends React.Component {
   constructor() {
     super()
     this.state = {
+      userid: this.props.match.params.id,
       enterprice: []
     }
   }
   async componentDidMount() {
+    try {
+      console.log(this.state.userid)
+      const { Etpdata } = await EtpInfoApi.getEnterpriseInfo(4)
+      console.log('hello')
+      console.log(Etpdata)
+    } catch (e) {
 
+    }
   }
   render() {
     const enterpriceItem = this.state.enterprice.map((item, idx) => {
--- src/components/common/enterprice-rmd/enterprice-item/EnterpriceItem.jsx
+++ src/components/common/enterprice-rmd/enterprice-item/EnterpriceItem.jsx
@@ -4,7 +4,7 @@ export default class EnterpriceItem extends React.Component {
   render() {
     const { enterprice } = this.props
     return (
-      <div styleName="enterprice_item">
+      <div styleName="enterprice-item">
         <img styleName="photo" src={enterprice.photo} />
         <span styleName="name" >{ enterprice.name }</span>
         <span styleName="type">{ enterprice.type }</span>
