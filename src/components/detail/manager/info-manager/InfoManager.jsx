e9e20ddd412524e61303fe959bebc35d60e66363
letch <953847229@qq.com>
2018-06-06 15:44:04 +0800

    详情：科研管理人员没有加好友 & 主页专利跳转详情

--- src/components/detail/manager/info-manager/InfoManager.jsx
+++ src/components/detail/manager/info-manager/InfoManager.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { AddFriendBtn } from 'components/common'
-import { Icon } from 'antd'
+import { Row, Col, Icon } from 'antd'
 import './infoManager.css'
 import imgView from 'src/assets/ico_eye.png'
 import imgAuth from './authority.png'
@@ -10,24 +9,25 @@ export default class InfoManager extends React.Component {
     const { manager } = this.props
     return (
       <div styleName="root">
-        <div>
-          <img src={imgView} />
-          <span styleName="view">{manager.pageView}</span>
-        </div>
-        <div styleName="detail">
-          <img styleName="avatar" src={manager.photo} />
-          <div styleName="name">
-            <span>{manager.realName}</span>
-            {manager.isValid ? <img src={imgAuth} style={{ verticalAlign: 'top' }} /> : ''}
-          </div>
-          <div styleName="basic">{manager.school}</div>
-          <div styleName="contact">
-            <Icon type="mail" styleName="contact-icon" /><span>{manager.email}</span>
-            <Icon styleName="contact-icon" type="wechat" /><span>{manager.wechat}</span>
-            <Icon type="qq" styleName="contact-icon" /><span>{manager.qq}</span>
-          </div>
-        </div>
-        <AddFriendBtn info={manager} />
+        <Row>
+          <Col span={8}>
+            <img src={imgView} />
+            <span styleName="view">{manager.pageView}</span>
+          </Col>
+          <Col span={8} styleName="detail">
+            <img styleName="avatar" src={manager.photo} />
+            <div styleName="name">
+              <span>{manager.realName}</span>
+              {manager.isValid ? <img src={imgAuth} style={{ verticalAlign: 'top' }} /> : ''}
+            </div>
+            <div styleName="basic">{manager.school}</div>
+            <div styleName="contact">
+              <Icon type="mail" styleName="contact-icon" /><span>{manager.email}</span>
+              <Icon styleName="contact-icon" type="wechat" /><span>{manager.wechat}</span>
+              <Icon type="qq" styleName="contact-icon" /><span>{manager.qq}</span>
+            </div>
+          </Col>
+        </Row>
       </div>
     )
   }
