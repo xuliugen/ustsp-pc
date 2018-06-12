e0d6c82191205e21c7b47a79d9aab7b5eec7e002
zero <zero@zerosrat.com>
2018-06-12 19:07:10 +0800

    chore(build): add uppfind api url for release ver.

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,7 +1,7 @@
 const config = {
   API_ORIGIN: 'http://xuliugen.vicp.io:8089/api',
+  // API_ORIGIN: 'http://www.uppfind.com/api',
   USA_ORIGIN: 'https://weixin.uppfind.com/university-assistant-server'
-
 }
 
 window.config = config
