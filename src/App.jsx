81ae039a3ab7e1d1559763332187287687f24cdd
zero <zerosrat@163.com>
2019-03-08 11:10:46 +0800

    refactor: aync load app.

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,23 @@ import { hot } from 'react-hot-loader'
 // import DevTools from 'mobx-react-devtools'
 
 import PrivateRoute from 'src/common/PrivateRoute'
-// import asyncComponent from 'src/common/AsyncComponent'
 import { ModalButton } from 'components/common'
+import asyncComponent from 'src/common/AsyncComponent'
 
 import Home from '@/home/Home'
 import Login from '@/login/Login'
 import ForgetPassword from '@/password/ForgetPassword'
 import Register from '@/register/Register'
-import Detail from '@/detail/Detail'
-import Search from '@/search/Search'
-import NewsDetail from '@/news/NewsDetail'
-import AllNews from '@/news/all-news/AllNews'
-import Admin from '@/admin/Admin'
-// const AsyncAdmin = asyncComponent(() => import('@/admin/Admin'))
+// import AllNews from '@/news/all-news/AllNews'
+// import Detail from '@/detail/Detail'
+// import Search from '@/search/Search'
+// import NewsDetail from '@/news/NewsDetail'
+// import Admin from '@/admin/Admin'
+const AsyncAdmin = asyncComponent(() => import('@/admin/Admin'))
+const AsyncNewsDetail = asyncComponent(() => import('@/news/NewsDetail'))
+const AsyncDetail = asyncComponent(() => import('@/detail/Detail'))
+const AsyncSearch = asyncComponent(() => import('@/search/Search'))
+const AsyncAllNews = asyncComponent(() => import('@/news/all-news/AllNews'))
 
 @withRouter
 @inject('msgStore', 'userStore')
@@ -48,17 +52,17 @@ class App extends React.Component {
           <Route path="/login" component={Login} />
           <Route path="/forget-pwd" component={ForgetPassword} />
           <Route path="/register" component={Register} />
-          <PrivateRoute path="/admin" component={Admin} />
-          {/* <Route path="/admin" component={Admin} /> */}
-          <Route path="/teacher/:id" component={Detail} />
-          <Route path="/student/:id" component={Detail} />
-          <Route path="/project/:id" component={Detail} />
-          <Route path="/news/:id" component={NewsDetail} />
-          <Route path="/ip/:id" component={Detail} />
-          <Route path="/enterprise/:id" component={Detail} />
-          <Route path="/manager/:id" component={Detail} />
-          <Route path="/search" component={Search} />
-          <Route path="/news" component={AllNews} />
+          {/* <PrivateRoute path="/admin" component={Admin} /> */}
+          <PrivateRoute path="/admin" component={AsyncAdmin} />
+          <Route path="/teacher/:id" component={AsyncDetail} />
+          <Route path="/student/:id" component={AsyncDetail} />
+          <Route path="/project/:id" component={AsyncDetail} />
+          <Route path="/news/:id" component={AsyncNewsDetail} />
+          <Route path="/ip/:id" component={AsyncDetail} />
+          <Route path="/enterprise/:id" component={AsyncDetail} />
+          <Route path="/manager/:id" component={AsyncDetail} />
+          <Route path="/search" component={AsyncSearch} />
+          <Route path="/news" component={AsyncAllNews} />
           <Redirect from="/" to="/" />
         </Switch>
         {/* <DevTools /> */}
